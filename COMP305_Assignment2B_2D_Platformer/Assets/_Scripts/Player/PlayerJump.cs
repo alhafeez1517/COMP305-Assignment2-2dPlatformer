3df255e8a91c4e0a986ee1ae0f567621d14aded9
alhafeez1517 <aragornlotr954@gmail.com>
2019-10-23 19:15:10 -0400

    Basic Movement and Jumping
    Implemented script for player movement and jumping

---
+++ COMP305_Assignment2B_2D_Platformer/Assets/_Scripts/Player/PlayerJump.cs
@@ -0,0 +1,92 @@
+﻿//Program Name:PlayerJump
+//Program Description:Program to make the player jump by 
+//applying force to rigidbody
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    [Header("Player Jump Variables")]
+    public float JumpVelocity;
+    public bool IsGrounded = false;
+    public float defaultGravity;
+    //public float gravityScaleRate;
+
+    [Header("Other")]
+    public Rigidbody2D PlayerRig;
+    private double gravity;
+    private float playerMass;
+    public bool SpaceDown;
+
+    void Start()
+    {
+       gravity = this.PlayerRig.gravityScale;
+       playerMass = this.PlayerRig.mass;
+    }
+
+    void FixedUpdate()
+    {
+        Jump();
+        
+    }
+
+    void Update()
+    {
+        KeyStatesCheck();
+        AlterGravity();
+        if (transform.position.y > 0.0125364)
+        {
+            IsGrounded = false;
+        }
+      
+       
+    }
+
+    public void KeyStatesCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpaceDown = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            SpaceDown = false;
+        } 
+        
+    }
+
+    public void Jump()
+    {
+        if (SpaceDown&&IsGrounded)
+        {
+            PlayerRig.AddForce(transform.up * JumpVelocity, ForceMode2D.Impulse);
+            IsGrounded = false;
+            
+        }
+
+    }
+
+    public void AlterGravity()
+    {
+        if (!IsGrounded)
+        {
+            PlayerRig.gravityScale += playerMass;
+        }
+        else
+        {
+            PlayerRig.gravityScale = defaultGravity;
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            IsGrounded = true;
+            gravity = defaultGravity;
+        }
+       
+    }
+}
---
+++ COMP305_Assignment2B_2D_Platformer/Assets/_Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿//Program Name:PlayerMovement.cs
+//Program Description:Program to move the player left and right by applying
+//force to the rigidbody
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Player Movement Variables")]
+    public float moveVelocity;
+    public Rigidbody2D playerRig;
+
+    void Start()
+    {
+        
+    }
+
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Movement()
+    {
+        float MoveHorizontal = Input.GetAxisRaw("Horizontal");
+
+        if (MoveHorizontal > 0.0)
+        {
+            playerRig.AddForce(transform.right * moveVelocity, ForceMode2D.Force);
+        }
+
+        if (MoveHorizontal < 0.0)
+        {
+            playerRig.AddForce(-transform.right * moveVelocity, ForceMode2D.Force);
+        }
+    }
+}
